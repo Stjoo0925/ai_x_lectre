73db068ecee432192221f7e8693a8c7a09d4788b
stjoo0925 <stjoo0925@gmail.com>
2024-10-26 17:00:28 +0900

    feat: Image Classifier with MediaPipe Tasks

---
+++ index.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+print("hello world")
+
+# 가상환경 목록 조회
+# conda env list
+
+# 가상환경 만들기
+# conda create -n project1 python=3.12
+
+# 가상환경 진입하기
+# conda activate project1
+
+# 가상환경 나가기
+# conda deactivate
+
+# 가상환경 삭제하기
+# conda remove -n project1
+
+# Vectorizing 패키지
+# pip install numpy
