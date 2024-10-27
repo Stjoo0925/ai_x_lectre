fc72c2baad8850d58ace0bad4ace3e3b4aaf2511
stjoo0925 <stjoo0925@gmail.com>
2024-10-27 15:43:48 +0900

    fix : fastapi image module updated

--- project1/api_cls.py
+++ project1/api_cls.py
@@ -3,21 +3,62 @@
 
 # pip install "fastapi[standard]"
 
-from typing import Union
+# STEP 1: Import modules.
+import numpy as np
+import cv2
+from fastapi import FastAPI, UploadFile
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python.components import processors
+from mediapipe.tasks.python import vision
 
-from fastapi import FastAPI
+# STEP 2: Create an ImageClassifier object.
+base_options = python.BaseOptions(
+    model_asset_path='models\efficientnet_lite0.tflite')
+options = vision.ImageClassifierOptions(
+    base_options=base_options, max_results=3)
+classifier = vision.ImageClassifier.create_from_options(options)
 
+# FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    contents = await file.read()
 
+    # contents <- burger.jpg(bytes)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # STEP 3: Load the input image.
+    binary = np.fromstring(contents, dtype=np.uint8)
+    cv_mat = cv2.imdecode(binary, cv2.IMREAD_COLOR)
+
+    # cv_mat = cv2.imread(contents)
+    # cv2.imread
+    # file = file.open('path')
+    # cv_mat = cv2.imdecode(file)
+
+    rgb_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv_mat)
+
+    # 흑백이미지 파일
+    # gray_frame = mp.Image(
+    #     image_format=mp.ImageFormat.GRAY8,
+    #     data=cv2.cvtColor(cv_mat, cv2.COLOR_RGB2GRAY))
+
+    # image = mp.Image.create_from_file('image\\burger.jpg')
+
+    # mp.Image.create_from_file : burger.jpg -> decoding(bitmap) -> convert to tensor
+
+    # STEP 4: Classify the input image.
+    classification_result = classifier.classify(rgb_frame)
+    # print(classification_result)
+
+    # STEP 5: Process the classification result. In this case, visualize it.
+    top_category = classification_result.classifications[0].categories[0]
+    print(f"{top_category.category_name} ({top_category.score:.2f})")
+
+    return {"category": top_category.category_name,
+            "score": f"{top_category.score:.2f}"}
 
 # fastapi dev api_cls.py
 
--- project1/cls.py
+++ project1/cls.py
@@ -10,28 +10,35 @@
 
 # import urllib.request
 
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.components import processors
+from mediapipe.tasks import python
+import mediapipe as mp
+import math
+import cv2  # opencv lib
 IMAGE_FILENAMES = ['image/burger.jpg', 'image/cat.jpg']
 
 # for name in IMAGE_FILENAMES:
 #   url = f'https://storage.googleapis.com/mediapipe-tasks/image_classifier/{name}'
 #   urllib.request.urlretrieve(url, name)
 
-import cv2 # opencv lib
 # from google.colab.patches import cv2_imshow
-import math
 
 DESIRED_HEIGHT = 480
 DESIRED_WIDTH = 480
 
+
 def resize_and_show(image):
-  h, w = image.shape[:2]
-  if h < w:
-    img = cv2.resize(image, (DESIRED_WIDTH, math.floor(h/(w/DESIRED_WIDTH))))
-  else:
-    img = cv2.resize(image, (math.floor(w/(h/DESIRED_HEIGHT)), DESIRED_HEIGHT))
+    h, w = image.shape[:2]
+    if h < w:
+        img = cv2.resize(
+            image, (DESIRED_WIDTH, math.floor(h/(w/DESIRED_WIDTH))))
+    else:
+        img = cv2.resize(
+            image, (math.floor(w/(h/DESIRED_HEIGHT)), DESIRED_HEIGHT))
 #   cv2_imshow(img)
-  cv2.imshow("test", img)
-  cv2.waitKey(0)
+    cv2.imshow("test", img)
+    cv2.waitKey(0)
 
 
 # Preview the images.
@@ -43,19 +50,16 @@ def resize_and_show(image):
 # Running inference and visualizing the results
 
 # STEP 1: Import the necessary modules.
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python.components import processors
-from mediapipe.tasks.python import vision
 
 # STEP 2: Create an ImageClassifier object.
-base_options = python.BaseOptions(model_asset_path='models\efficientnet_lite0.tflite')
+base_options = python.BaseOptions(
+    model_asset_path='models\efficientnet_lite0.tflite')
 options = vision.ImageClassifierOptions(
     base_options=base_options, max_results=3)
 classifier = vision.ImageClassifier.create_from_options(options)
 
 # STEP 3: Load the input image.
-image = mp.Image.create_from_file('burger.jpg')
+image = mp.Image.create_from_file('image\\burger.jpg')
 
 # STEP 4: Classify the input image.
 classification_result = classifier.classify(image)
@@ -66,14 +70,14 @@ classification_result = classifier.classify(image)
 #   classifications=[
 #     Classifications(
 #       categories=[
-#         Category(index=933, score=0.9790240526199341, display_name='', category_name='cheeseburger'), 
-#         Category(index=931, score=0.0008637145510874689, display_name='', category_name='bagel'), 
+#         Category(index=933, score=0.9790240526199341, display_name='', category_name='cheeseburger'),
+#         Category(index=931, score=0.0008637145510874689, display_name='', category_name='bagel'),
 #         Category(index=947, score=0.0005722717614844441, display_name='', category_name='mushroom')
-#       ], 
-#       head_index=0, 
+#       ],
+#       head_index=0,
 #       head_name='probability'
 #     )
-#   ], 
+#   ],
 #   timestamp_ms=0
 # )
 
