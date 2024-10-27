860f5513d7cc69a21398796cd2845a225ddf46a0
stjoo0925 <stjoo0925@gmail.com>
2024-10-27 12:49:33 +0900

    feat: text_classification.py update

---
+++ project3/main.py
@@ -0,0 +1,12 @@
+# huggingface | transformers
+# https://huggingface.co/docs/transformers/installation
+# pip install transformers
+
+# pytorch
+# https://pytorch.org/
+# stable | window | python | cpu 체크
+# conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
+# 자습용 예제
+# huggingface / transformers / docs
+# https://huggingface.co/docs/transformers/ -> Task Guides
---
+++ project3/text_classification.py
@@ -0,0 +1,18 @@
+# Text classification
+# https://huggingface.co/docs/transformers/tasks/sequence_classification
+
+# STEP 1 : import module
+from transformers import pipeline
+
+# STEP 2 : create inference object(instance)
+classifier = pipeline("sentiment-analysis",
+                      model="sangrimlee/bert-base-multilingual-cased-nsmc")
+
+# STEP 3 : prepare data
+text = "도대체 뭘 만드신거에요?"
+
+# STEP 4 : inference
+result = classifier(text)
+
+# STEP 5 : post processing
+print(result)
